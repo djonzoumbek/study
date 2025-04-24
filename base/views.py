4ec6ba42864d41d88c3ef5a58db41c0e7da9b39d
djonzoumbek <guepelbekamis@gmail.com>
2025-04-24 22:16:12 +0000

    Mise en place de la fonctionnalité de recherche de salon par nom, catégorie et par description

--- base/views.py
+++ base/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 
 
 
 def home(request):
-    rooms = Room.objects.all()
-    return render(request, 'base/home.html', {'rooms': rooms})
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+    room_count = rooms.count()
+    topics = Topic.objects.all()
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
