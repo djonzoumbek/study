b0ac9aa047ff8161785123aa3d62bb0bc3f3d55b
djonzoumbek <guepelbekamis@gmail.com>
2025-04-24 18:55:55 +0000

    Mise en place de la première migration et des premières models

--- base/views.py
+++ base/views.py
@@ -1,27 +1,15 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Room
 
 
 
-#rooms
-rooms = [
-    {"id" : 1, "name" : "Let's learn Python"},
-    {"id" : 2, "name" : "Django for Beginners"},
-    {"id" : 3, "name" : "HTML & CSS"},
-    {"id" : 4, "name" : "JavaScript"},
-    {"id" : 5, "name" : "React for Beginners"},
-    {"id" : 6, "name" : "All about VSC"},
-    {"id" : 7, "name" : "GitHub"},
-]
+
 
 def home(request):
+    rooms = Room.objects.all()
     return render(request, 'base/home.html', {'rooms': rooms})
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html',  context)
