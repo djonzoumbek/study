d347f0ed64bcdae82e229220a369655a3c0a974d
djonzoumbek <guepelbekamis@gmail.com>
2025-04-27 06:38:40 +0000

    mise en place de la fonctionnalité de messsagerie par salon

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 
@@ -76,7 +76,17 @@ def logout_user(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body = request.POST.get('body')
+        )
+
+        return redirect('room', pk=room.id)
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 
